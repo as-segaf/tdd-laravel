b7af6af32b608f70cedea84284d3afaec513c0e9
as-segaf <ziyanassegaf20@gmail.com>
2019-09-28 14:56:04 +0700

    second test

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -8,18 +8,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
-    
+    // use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    // public function testExample()
-    // {
-    //     $response = $this->get('/');
+    public function testExample()
+    {
+        $response = $this->get('/');
 
-    //     $response->assertStatus(200);
-    // }
+        $response->assertStatus(200);
+    }
 
     /**
      * @test
@@ -33,7 +33,24 @@ class TaskTest extends TestCase
         $response = $this->get('/tasks');
 
         //he should be able to reaed the tasks
-        $response->assertStatus(200);
-        $response->assertSee($task->title);
+        $response->assertStatus(200)
+            ->assertSee($task->title);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_a_single_task()
+    {
+        //given we have task in the database
+        $task = factory('App\Task')->create();
+
+        //when user visits the task's URI
+        $response = $this->get('/tasks/'.$task->id);
+
+        //he should be able to read the task's details
+        $response->assertStatus(200)
+            ->assertSee($task->title)
+            ->assertSee($task->desctiption);
     }
 }
